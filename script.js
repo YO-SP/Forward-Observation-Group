bf1f72ba30fb088d9dc2afb457ab0451fc5609aa
Yakobus Octavianus Sucahyo Putra <yakobusavin2004@gmail.com>
2025-06-25 22:14:54 +0700

    Commit 1 : Struktur Mentahan

---
+++ script.js
@@ -0,0 +1,9 @@
+const action_img = document.getElementById('action_pack');
+const images = action_img.querySelectorAll('img');
+
+images.forEach(function(el){
+    el.addEventListener('click', function(e){
+        window.open(el.src, '_blank');
+    });
+});
+
